11a49a02277496eb6da888826a0df67f9d0696a1
gdky005 <741227905@qq.com>
2019-04-25 00:32:27 +0800

    1. 添加用户登录和注销逻辑，显示不同的 view。

--- app/src/main/java/com/zk/gaokaopro/fragment/MeFragment.kt
+++ app/src/main/java/com/zk/gaokaopro/fragment/MeFragment.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.blankj.utilcode.util.BarUtils
 import com.zk.gaokaopro.R
 import com.zk.gaokaopro.activity.AboutMeActivity
 import com.zk.gaokaopro.activity.LoginActivity
+import com.zk.gaokaopro.manager.UserInfoManager
 import kotlinx.android.synthetic.main.fragment_me.*
 import team.zhuoke.sdk.base.BaseFragment
 
@@ -15,6 +19,10 @@ class MeFragment : BaseFragment(), View.OnClickListener {
 
     lateinit var btLogin: Button
     lateinit var btAboutMe: Button
+    lateinit var btLogout: Button
+    lateinit var llLogin: LinearLayout
+    lateinit var userLogo: ImageView
+    lateinit var userName: TextView
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_me
@@ -25,18 +33,50 @@ class MeFragment : BaseFragment(), View.OnClickListener {
 
         btLogin = rootView.findViewById(R.id.btLogin)
         btAboutMe = rootView.findViewById(R.id.btAboutMe)
+        btLogout = rootView.findViewById(R.id.btLogout)
+        llLogin = rootView.findViewById(R.id.ll_login)
+        userLogo = rootView.findViewById(R.id.userLogo)
+        userName = rootView.findViewById(R.id.userName)
     }
 
     override fun initListener() {
         btLogin.setOnClickListener(this)
         btAboutMe.setOnClickListener(this)
+        btLogout.setOnClickListener(this)
     }
 
     override fun initData(savedInstanceState: Bundle?) {
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        refreshUserLogin()
+    }
+
+    private fun refreshUserLogin() {
+        if (UserInfoManager.instance.isLogin()) {
+            btLogin.visibility = View.GONE
+            btLogout.visibility = View.VISIBLE
+            userName.visibility = View.VISIBLE
+
+            userName.text = UserInfoManager.instance.getUserName()
+
+        } else {
+            btLogin.visibility = View.VISIBLE
+            btLogout.visibility = View.GONE
+            userName.visibility = View.GONE
+        }
+    }
+
 
     private fun login() {
         startActivity(Intent(activity, LoginActivity::class.java))
+    }
+
+    private fun logout() {
+        UserInfoManager.instance.clear()
+        refreshUserLogin()
     }
 
     private fun aboutMe() {
@@ -52,6 +92,10 @@ class MeFragment : BaseFragment(), View.OnClickListener {
 
             R.id.btAboutMe -> {
                 aboutMe()
+            }
+
+            R.id.btLogout -> {
+                logout()
             }
             else -> {
 
